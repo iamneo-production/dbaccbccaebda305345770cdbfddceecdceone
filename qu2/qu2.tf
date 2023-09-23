63e375382518b7379b1cebf52d232ecba939b194
Iamneo <techadmin@iamneo.ai>
2023-09-23 06:39:00 +0000

    Backup Commit

---
+++ qu2/qu2.tf
@@ -0,0 +1,24 @@
+terraform {
+    required_providers {
+      aws = {
+          source = "hashicorp/aws" 
+          version = "~>4.0"
+      }
+    }
+    
+} 
+
+provider "aws"{
+    region = "" 
+    access_key = "" 
+    secret_key = ""
+} 
+
+resource "aws_instance" "linux_server" {
+    ami="" 
+    instance_type = "t2.micro"
+} 
+
+output "public_ip"{
+    value = aws_instance.linux_server.public_ip
+}
